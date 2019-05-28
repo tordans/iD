d65eaa96ebf15755a4a676e760cfce4b5693147e
Quincy Morgan <quincy.morgan@icloud.com>
2019-05-28 14:24:16 -0400

    Add Downgrade operation to the top toolbar

--- modules/ui/top_toolbar.js
+++ modules/ui/top_toolbar.js
@@ -17,7 +17,7 @@ export function uiTopToolbar(context) {
         undoRedo = uiToolUndoRedo(context),
         save = uiToolSave(context);
 
-    var supportedOperationIDs = ['circularize', 'delete', 'disconnect', 'merge', 'orthogonalize', 'split', 'straighten'];
+    var supportedOperationIDs = ['circularize', 'downgrade', 'delete', 'disconnect', 'merge', 'orthogonalize', 'split', 'straighten'];
 
     var operationToolsByID = {};
 
@@ -60,7 +60,7 @@ export function uiTopToolbar(context) {
             for (var i in operations) {
                 var operation = operations[i];
                 var tool = operationTool(operation);
-                if (operation.id !== 'delete') {
+                if (operation.id !== 'delete' && operation.id !== 'downgrade') {
                     operationTools.push(tool);
                 } else {
                     deleteTool = tool;
