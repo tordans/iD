6a0b0d0b18053f03f76cac2b703bb2120784a255
Tobias Jordans <t@tobiasjordans.de>
2019-07-20 13:34:50 +0200

    Fix part of the .fromExternal-Code but not all fixed

--- modules/core/context.js
+++ modules/core/context.js
@@ -604,7 +604,7 @@ export function coreContext() {
     if (presetsParameter && presetsParameter.indexOf('://') !== -1) {
         // assume URL of external presets file
 
-        presets.fromExternal(external, function(externalPresets) {
+        presets.fromExternal(presetsParameter, function(externalPresets) {
             context.presets = function() { return externalPresets; }; // default + external presets...
             osmSetAreaKeys(presets.areaKeys());
             osmSetPointTags(presets.pointTags());
