84e1b3325d9cc3baddaf855cc953a87cfac4623a
Quincy Morgan <quincy.morgan@icloud.com>
2019-08-19 16:14:22 -0500

    Use X button instead of "Continue Mapping" button to close save success screen (re:  #6768) Increase assistant padding somewhat

--- modules/ui/assistant.js
+++ modules/ui/assistant.js
@@ -863,7 +863,11 @@ export function uiAssistant(context) {
             theme: 'light',
             headerIcon: savedIcon,
             title: t('assistant.commit.success.thank_you'),
-            collapseCategory: 'save'
+            collapseCategory: 'save',
+            onClose: function() {
+                updateDidEditStatus();
+                redraw();
+            }
         };
 
         panel.renderHeaderBody = function(selection) {
@@ -894,15 +898,6 @@ export function uiAssistant(context) {
 
             link
                 .call(svgIcon('#iD-icon-out-link', 'inline'));
-
-            mainFooter.append('button')
-                .attr('class', 'primary')
-                .on('click', function() {
-                    updateDidEditStatus();
-                    redraw();
-                })
-                .append('span')
-                .text(t('assistant.commit.continue_mapping'));
         };
 
         panel.renderBody = function(selection) {
